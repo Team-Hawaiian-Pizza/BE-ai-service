94761d8ef2ee2b8e546349d1233340fcc2d9ed6e
dohyeong <daniel010309@gmail.com>
2025-08-19 13:38:35 +0900

    Feat: Add Allowed_Hosts

--- AI_service/settings.py
+++ AI_service/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'be-ai-service-yor9.onrender.com',
+]
 
 
 # Application definition
